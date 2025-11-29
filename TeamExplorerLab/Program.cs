c3ca10d659718f7fea9edeb8362b9bb4419bc85e
Abdulla Arzimurotov <aarzimurotov2001@gmail.com>
2025-11-29 14:32:46 +0300

    Переведено на русский язык

--- TeamExplorerLab/Program.cs
+++ TeamExplorerLab/Program.cs
@@ -6,36 +6,36 @@ namespace TeamExplorerLab
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Nuqtaning koordinatalarini kiriting (x, y):");
+            Console.WriteLine("Введите координаты точки (x, y):");
             Console.Write("x = ");
             double x = Convert.ToDouble(Console.ReadLine());
             Console.Write("y = ");
             double y = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Aylanalar radiuslarini kiriting (r1, r2):");
+            Console.WriteLine("Введите радиусы окружностей (r1, r2):");
             Console.Write("r1 = ");
             double r1 = Convert.ToDouble(Console.ReadLine());
             Console.Write("r2 = ");
             double r2 = Convert.ToDouble(Console.ReadLine());
 
-            // Katta va kichik radiusni aniqlab olamiz
+            // Определяем больший и меньший радиус
             double R = Math.Max(r1, r2);
             double r = Math.Min(r1, r2);
 
-            // Nuqtaning markazdan masofasini hisoblaymiz
+            // Вычисляем расстояние от точки до центра координат
             double d = Math.Sqrt(x * x + y * y);
 
             if (d > r && d < R)
             {
-                Console.WriteLine("Nuqta soha ICHIDA yotadi.");
+                Console.WriteLine("Точка находится ВНУТРИ области.");
             }
             else if (d < r || d > R)
             {
-                Console.WriteLine("Nuqta soha TASHQARISIDA yotadi.");
+                Console.WriteLine("Точка находится ВНЕ области.");
             }
             else
             {
-                Console.WriteLine("Nuqta soha CHEGARASIDA yotadi.");
+                Console.WriteLine("Точка находится НА ГРАНИЦЕ области.");
             }
 
             Console.ReadKey();
